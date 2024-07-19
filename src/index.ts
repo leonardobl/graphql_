9d44dc849febbcad9644d2c1f0469dca6b4fe516
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-07-19 05:38:58 -0300

    ajuste tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello wold');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `,
+
+  resolvers: {
+    Query: {
+      hello() {
+        return 'Hello Wold';
+      },
+    },
+  },
+});
+
+server
+  .listen(4003)
+  .then(() => console.log('Server On ...'))
+  .catch((error) => console.log(error));
